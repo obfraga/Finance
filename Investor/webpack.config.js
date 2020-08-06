ebf1f5f7b3ecb5b4239ed3cd35a01716caaf7a01
Bruno Fraga <bruno@remotatec.com.br>
2020-08-06 11:16:11 -0300

    Implementing webpack and basic packages.

---
+++ Investor/webpack.config.js
@@ -0,0 +1,45 @@
+﻿const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty',
+    },
+    devtool: false,
+    name: 'general',
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+                loader: 'url-loader',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    entry: {
+        site: './wwwroot/js/site.js',
+    },
+    output: {
+        path: path.resolve(__dirname + "/wwwroot/", 'js'),
+        filename: '[name].bundle.js',
+    },
+    plugins: ([
+        new MiniCssExtractPlugin({
+            filename: '../css/[name].css'
+        }),
+    ]),
+};
--- Investor/wwwroot/js/site.js
+++ Investor/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+import '../css/site.scss';
+
+
+var yahooFinance = require('yahoo-finance');
+
+yahooFinance.historical({
+    symbol: 'AAPL',
+    from: '2012-01-01',
+    to: '2012-12-31',
+    // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
+}, function (err, quotes) {
+    alert("a");
+});
+
+// This replaces the deprecated snapshot() API
+yahooFinance.quote({
+    symbol: 'AAPL',
+    modules: ['price', 'summaryDetail'] // see the docs for the full list
+}, function (err, quotes) {
+    alert("b");
+});
