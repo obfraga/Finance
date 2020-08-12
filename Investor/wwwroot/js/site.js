b2bbe6f558d7b3e7ca1df6e4db0b9e476489e392
Bruno Fraga <bruno@remotatec.com.br>
2020-08-12 12:56:47 -0300

    Retirando yahoo-finance dos pacotes utilizados e adicionando scripts a serem executados para dar "compilar" o webpack antes de executar a aplicação.

--- Investor/wwwroot/js/site.js
+++ Investor/wwwroot/js/site.js
@@ -5,22 +5,4 @@
 
 import '../css/site.scss';
 
-
-var yahooFinance = require('yahoo-finance');
-
-yahooFinance.historical({
-    symbol: 'AAPL',
-    from: '2012-01-01',
-    to: '2012-12-31',
-    // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
-}, function (err, quotes) {
-    alert("a");
-});
-
-// This replaces the deprecated snapshot() API
-yahooFinance.quote({
-    symbol: 'AAPL',
-    modules: ['price', 'summaryDetail'] // see the docs for the full list
-}, function (err, quotes) {
-    alert("b");
-});
+//--- Request to https://finance.yahoo.com/quote/AAPL
